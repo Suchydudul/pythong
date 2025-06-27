4426dbbddacc75aad15fbe6cdfc0ff179e13d5dc
dudul <ddzero666@gmail.com>
2025-06-27 23:45:26 +0200

    added some archaic encryption

--- functions/encryption.py
+++ functions/encryption.py
@@ -1,2 +1,18 @@
 import random
 import string
+
+chars = string.punctuation + string.digits + string.ascii_letters + " "
+
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key {key}")
+encrypted_user_input = ""
+user_input = input("Please write your message to encrypt: ")
+for letter in user_input:
+    index = chars.index(letter)
+    encrypted_user_input += key[index]
+
+print(encrypted_user_input)

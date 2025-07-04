d16609bf2f0584f1c611951e9e431f5c98f36a99
dudul <ddzero666@gmail.com>
2025-07-04 21:39:39 +0200

    Gui making

---
+++ PyQt5_gui/first_gui.py
@@ -0,0 +1,35 @@
+import sys
+import os
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__ (self):
+        super().__init__()
+
+
+        self.setWindowTitle("Testing GUI")
+        self.setWindowIcon(QIcon("/home/_dudul/python/pythong-1/PyQt5_gui/images.png"))
+        self.setGeometry(0,0,500,500)
+
+        label = QLabel("Hello ", self)
+        label.setFont(QFont("Lora", 30))
+        label.setGeometry(0,0,500,500)
+        label.setStyleSheet("color: blue")
+        label.setAlignment(Qt.AlignCenter)
+
+        pixmap = QPixmap("/home/_dudul/python/pythong-1/PyQt5_gui/images.png")
+        label.setPixmap(pixmap)
+        label.setScaledContents(True)
+
+def main():
+    if os.path.exists("/home/_dudul/python/pythong-1/PyQt5_gui/icon.jpeg"):
+        print("exist")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()

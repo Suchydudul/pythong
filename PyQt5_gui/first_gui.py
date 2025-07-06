e43b1f4b0c036a759e5f71e0e8f09d2dc517f2d2
dudul <ddzero666@gmail.com>
2025-07-06 12:57:51 +0200

    updating clock not working

--- PyQt5_gui/first_gui.py
+++ PyQt5_gui/first_gui.py
@@ -1,31 +1,47 @@
 import sys
 import os
+import time
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
 from PyQt5.QtGui import QIcon, QFont, QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 
 class MainWindow(QMainWindow):
+    def update_time(self):
+        current_time = time.asctime()[11:19]
+        self.label.setText(f"Current time: {current_time}")
+    
     def __init__ (self):
         super().__init__()
 
-
+        current_date = time.asctime()
         self.setWindowTitle("Testing GUI")
         self.setWindowIcon(QIcon("/home/_dudul/python/pythong-1/PyQt5_gui/images.png"))
         self.setGeometry(0,0,500,500)
 
-        label = QLabel("Hello ", self)
-        label.setFont(QFont("Lora", 30))
-        label.setGeometry(0,0,500,500)
-        label.setStyleSheet("color: blue")
-        label.setAlignment(Qt.AlignCenter)
 
-        pixmap = QPixmap("/home/_dudul/python/pythong-1/PyQt5_gui/images.png")
-        label.setPixmap(pixmap)
-        label.setScaledContents(True)
+        self.label = QLabel(f"Current time: {current_date[11:19]}", self)
+        self.label.setFont(QFont("Lora", 30))
+        self.label.setGeometry(0,0,500,500)
+        self.label.setStyleSheet("color: blue")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+
+
+
+
+
+     
+
+        #pixmap = QPixmap("/home/_dudul/python/pythong-1/PyQt5_gui/images.png")
+        #label.setPixmap(pixmap)
+        #label.setScaledContents(True)
 
 def main():
-    if os.path.exists("/home/_dudul/python/pythong-1/PyQt5_gui/icon.jpeg"):
-        print("exist")
+    current_date = time.asctime()
+    print(current_date[11:19])
     app = QApplication(sys.argv)
     app.setApplicationDisplayName("AlarmApp")
     app.setDesktopFileName("AlarmApp")
